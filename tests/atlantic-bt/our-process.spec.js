098b9d8e64d1aa204ef6877d62bc7cc96206e6e3
Ryan Altvater <ryan@ryanaltvater.com>
2023-07-28 10:39:47 -0400

    Updated example code

--- tests/atlantic-bt/our-process.spec.js
+++ tests/atlantic-bt/our-process.spec.js
@@ -1,11 +1,37 @@
 const screenWidths = [576, 768, 992, 1200];
 
 describe("Our Process page", () => {
-    it("should visit the 'Our Process' page", () => {
+    beforeEach(() => {
         cy.visit("https://www.atlanticbt.com/our-process");
+    });
 
+    it("should visit the 'Our Process' page", () => {
         cy.percySnapshot('Visited the "Our Process" page', {
             widths: screenWidths
         });
+    });
+
+    // Checks a link
+    it("should test the Atlantic BT logo link", () => {
+        cy.get(".site-logo__link")
+            .should("have.attr", "href", "https://www.atlanticbt.com")
+            .should("have.attr", "aria-label", "Atlanticbt.com");
+    });
+
+    // Checks some text (fails on purpose as a demonstration)
+    it("should check the 'Contact' button text", () => {
+        cy.get(".nav-desktop__link.js-menu-contact").should(
+            "have.text",
+            "Contact us"
+        );
+    });
+
+    // Checks a color
+    it("should have purple subtitles", () => {
+        cy.get(".band__subtitle").should(
+            "have.css",
+            "color",
+            "rgb(87, 8, 211)"
+        );
     });
 });
--- tests/training-industry/contact.spec.js
+++ tests/training-industry/contact.spec.js
@@ -1,11 +1,17 @@
 const screenWidths = [576, 768, 992, 1200];
 
 describe("Contact page", () => {
-    it("should visit the 'Contact' page", () => {
+    beforeEach(() => {
         cy.visit("https://trainingindustry.com/contact");
+    });
 
+    it("should visit the 'Contact' page", () => {
         cy.percySnapshot('Visited the "Contact" page', {
             widths: screenWidths
         });
+    });
+
+    it("should visit the 'Contact' page", () => {
+        cy.get("page__heading title--page").should("have.text", "Contact");
     });
 });
